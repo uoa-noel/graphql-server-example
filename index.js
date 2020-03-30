b78b7171207d1138df9d0dd0f5a4e01cef5b6d2c
Sam Kavanagh <hganavak@gmail.com>
2020-03-30 17:50:52 +1300

    Got a working GraphQL proxy. 1000th GitHub commit too

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const { ApolloServer, gql } = require('apollo-server');
+const { RESTDataSource } = require('apollo-datasource-rest');
+const { createApolloFetch } = require('apollo-fetch');
+
+
+// Apollo Library for querying other GraphQL endpoints
+const apolloFetch = createApolloFetch({
+  uri: 'https://api.spacex.land/graphql',
+});
 
 // Example data (replace with endpoint etc)
 const books = [
@@ -68,12 +76,19 @@ const typeDefs = gql`
         isPublic: Boolean
     }
 
+    # Stuff in an existing GraphQL endpoint test
+    type Launch {
+        mission_name: String
+    }
+
     # The Query type defines exactly which GraphQL queries (i.e., read operations) clients can execute against your data graph. It resembles an object type, but its name is always Query.
     # Each field of the Query type defines the name and return type of a different supported query. The Query type for our example schema might resemble the following:e
     type Query {
         books: [Book]
         articles: [Article]
+        launches: [Launch]
     }
+
 
 
     # Define Mutations here
@@ -100,7 +115,10 @@ const typeDefs = gql`
 const resolvers = {
     Query: {
         books: () => books,
-        articles: () => getArticles()
+        articles: () => getArticles(),
+        launches: async (_source, _args, { dataSources} ) => {
+            return dataSources.spacexAPI.getLaunches();
+        }
     },
 };
 
@@ -111,7 +129,15 @@ const resolvers = {
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+    typeDefs,
+    resolvers,
+    dataSources: () => { // These can now be accessed inside the resolvers
+        return { 
+            spacexAPI: new SpacexAPI() 
+        };
+    },
+});
 
 // The 'listen' method launches a web server.
 server.listen().then(({ url }) => {
@@ -121,6 +147,42 @@ server.listen().then(({ url }) => {
 
 // Method to call external GraphQL API/return response
 // https://www.apollographql.com/docs/apollo-server/data/data-sources/
-callExternal= (url, query) => {
+callExternal = (url, query) => {
     return "external data";
 }
+
+
+/*
+This might be handy ITS PART OF THE REQUEST NOT THE HEADERS@!!:
+
+    class PersonalizationAPI extends RESTDataSource {
+    willSendRequest(request) {
+        request.params.set('api_key', this.context.token);
+    }
+    }
+*/
+class SpacexAPI extends RESTDataSource {
+    constructor() {
+        super();
+        this.baseURL = 'https://api.spacex.land/graphql';
+    }
+
+    async getLaunches() {
+
+        // return this.get('/', )
+        // return [{ mission_name: 'example launch' }];
+        
+        // Proof of concept but apolloFetch is deprecated, don't use
+        // Use the RESTDataSource as above
+        return await apolloFetch({
+            query: `{ 
+                launches {
+                    mission_name
+                }
+            }`,
+        })
+        .then(res => [...res.data.launches]);
+                
+    };
+}
+
