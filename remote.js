d33c5f393eff14b0d0c0b607a32ed5b562d5a106
Sam Kavanagh <hganavak@gmail.com>
2020-04-01 10:23:47 +1300

    Tidied everything up a bit more

--- remote.js
+++ remote.js
@@ -3,41 +3,47 @@ const { HttpLink } = require('apollo-link-http');
 const { introspectSchema , makeRemoteExecutableSchema, mergeSchemas } = require('graphql-tools');
 const fetch = require('node-fetch');
 
-/**
- * Define constants
- */
-const link = new HttpLink({ uri: 'https://api.spacex.land/graphql', fetch });
+// Load a remote schema and set up the http-link
+getRemoteSchema = async(remoteUri) => {
+    try {
+        console.log('Loading remote schema:', remoteUri)
+        const link = new HttpLink({ uri: remoteUri, fetch });
+        const schema = await introspectSchema(link);
 
-getContentfulSchema = async() => {
-    const schema = await introspectSchema(link);
-  
-    const executableSchema = makeRemoteExecutableSchema({
-      schema,
-      link,
-    });
-  
-    return executableSchema
+        console.log('Loaded remote schema:', remoteUri)
+        return makeRemoteExecutableSchema({
+            schema,
+            link,
+        });
+    } catch(e) {
+        Console.error(e);
+    }
 }
 
-/**
- * Set up the schemas and initialize the server
- */
+// Set up the schemas and initialize the server
 initialize = async () => {
-    contentfulSchema = await getContentfulSchema();
 
-    // Load all schemas (remote and local) here
+    // Load remote schemas here
+    spacexSchema = await getRemoteSchema('https://api.spacex.land/graphql');
+    countriesSchema = await getRemoteSchema('https://countries.trevorblades.com/');
+
+    // Merge all schemas (remote and local) here
     const schema = mergeSchemas({
-        schemas: [contentfulSchema],
+        schemas: [
+            spacexSchema,
+            countriesSchema
+        ],
     });
 
+    // Create our ApolloServer object, and pass our merged schemas
     const server = new ApolloServer({
         schema
     });
 
+    // Listen up!
     server.listen().then(({ url }) => {
         console.log(`🚀  Server ready at ${url}`);
     });
-
 }
 
 initialize();
